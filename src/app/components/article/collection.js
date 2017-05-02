29f2c3972373b6954ac3b72ae158988cb379f5da
danielisaacgeslin <danielisaacgeslin@gmail.com>
2017-05-02 07:37:57 -0300

    fix

--- src/app/components/article/collection.js
+++ src/app/components/article/collection.js
@@ -1,20 +1,9 @@
-// import { Collection } from 'backbone';
-import * as Backbone from 'backbone';
+import { Collection } from 'backbone';
 import ArticleModel from './model';
 
-// export default class ArticleCollection extends Collection {
-//     constructor(params) {
-//         super(params);
-//     }
-
-//     model() {
-//         return ArticleModel;
-//     }
-// }
-
-const ArticleCollection = Backbone.Collection.extend({
-    model: ArticleModel,
-    url: './dummy.json'
-});
-
-export default ArticleCollection;
+export default class ArticleCollection extends Collection {
+    initialize(params) {
+        this.model = ArticleModel;
+        this.url = './dummy.json';
+    }
+}
